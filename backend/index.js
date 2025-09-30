543dcf2c20d62aaec31776d66f6403cdf93fa4b1
saberahmadmd <157378864+saberahmadmd@users.noreply.github.com>
2025-10-01 01:17:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://college-hub-dashboard.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
